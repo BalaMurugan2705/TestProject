67aa42b4590db379630cfb1396034009f2d26761
Balamurugan2705 <balamurugan.softsuave@gmail.com>
2023-04-27 19:49:35 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:test_project/bloc/user_cubit.dart';
-
 import 'helper/nav_helper.dart';
 import 'helper/nav_observer.dart';
 import 'helper/utils.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
